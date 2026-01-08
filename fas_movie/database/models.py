6b209adddc37cb3c326255e8214f780a1711a95e
Ryui-code <ulanbektalasbaev2020@gmail.com>
2026-01-08 20:29:58 +0600

    Hi!

--- fas_movie/database/models.py
+++ fas_movie/database/models.py
@@ -1,4 +1,4 @@
-from fas_movie.database.db import Base
+from .db import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Enum, Integer, String, Date, Text, ForeignKey
 from typing import List, Optional
